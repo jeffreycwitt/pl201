b4c44fdd6d47bd823072eb9f8edb174ca24b4a15
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2022-08-27 15:17:54 -0400

    added more questions; modified file names

--- components/Answer.js
+++ components/Answer.js
@@ -2,21 +2,20 @@ import {useState} from 'react'
 
 export default function Answer(props) {
   const [showAnswer, setShowAnswer] = useState(false)
-  const indent = props.level ? (props.level + 1) * 10 : 10
-  const mainClass = (!props.level || props.level === "1") && "main" 
+  
   return (
-    <div>
-      <p className="answer-toggle" onClick={() => {setShowAnswer(!showAnswer)}}>{showAnswer ? "hide" : "show suggestion"}</p>
-      {showAnswer && <div className={"answer " + props.type + " " + mainClass} style={{paddingLeft: indent + "px"}}>
-      {props.date}
+    <>
+      <span className="answer-toggle" onClick={() => {setShowAnswer(!showAnswer)}}>{showAnswer ? "hide suggestion" : "show suggestion"}</span>
+      {showAnswer && <div className={"answer"}>
       {props.children}
       </div>
       }
       <style jsx>{`
         
         .answer{
-          padding-right: 10px; 
-          background-color: lightgreen;
+          padding: 20px; 
+          background-color: #e6ffe6;
+          font-size: 20px;
 
         }
         .answer-toggle{
@@ -24,12 +23,13 @@ export default function Answer(props) {
           cursor: pointer;
           margin: 0;
           padding: 0;
+          padding-left: 10px;
         }
         .answer-toggle:hover{
           text-decoration: underline;
         }
 
       `}</style>
-    </div>
+    </>
   )
 }
