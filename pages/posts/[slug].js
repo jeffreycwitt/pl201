263ec665c3cda1e76da95f89bfece41e5eadfcef
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2022-09-02 17:14:26 -0400

    small change; trying to fix build process

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -24,7 +24,7 @@ const components = {
   Question: dynamic(() => import('../../components/Question')),
   Answer: dynamic(() => import('../../components/Answer')),
   QuestionBlock: dynamic(() => import('../../components/QuestionBlock')),
-  BabylonViewerWrapper: dynamic(() => import('../../components/BabylonViewerWrapper')),
+  BabylonViewerWrapper: dynamic(() => import('../../components/BabylonViewerWrapper'), {ssr: false}),
   Head,
   Image,
   
