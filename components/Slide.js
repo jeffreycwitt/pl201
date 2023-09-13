620ba43866345e82a8fe544b04ed58675b91c3e6
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2023-09-13 12:26:01 -0400

    slide updates

--- components/Slide.js
+++ components/Slide.js
@@ -7,7 +7,7 @@ export default function Slide(props) {
     <div className={"slide " + props.number}>
       {props.children}
       <style jsx>{`
-        .slide { width: 80%; height: 98vh; align-items: center; justify-content: center; font-size: 1.5em; border-bottom: .5px dotted black; padding: 0 10% }
+        .slide { width: 80%; height: 98vh; align-items: center; justify-content: center; font-size: 1.75em; border-bottom: .5px dotted black; padding: 0 10% }
       `}</style>
     </div>
   )
